d5c12381b61cd0b75c8550033b1bea5005ae5818
oreo921 <103257421+oreo921@users.noreply.github.com>
2022-07-29 17:40:45 +0900

    Update HeadsOrTails.java
    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -17,8 +17,14 @@ public class HeadsOrTails {
       int heads = 0;
       int tails = 0;
       int chances = 3;
+      
+      System.out.println("Who are you?");
+      Scanner scan = new Scanner(System.in);
+      String str = scan.next();
+      System.out.println("Hello,"+str);
       System.out.println("Tossing a coin...");
-      for (int i = 1; i<= chances; i++) {
+      
+	   for (int i = 1; i<= chances; i++) {
 	  System.out.print("Round"+i+":");
          if (t.chanceFunc().equals("tails")) {
             tails++;

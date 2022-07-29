bb453da5e1a3cb549f6704f74d2ab5e0a1ec490e
reore <reoreo14730@docomo.ne.jp>
2022-07-29 17:04:31 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+class Toss {
+   public String chanceFunc() {
+      Random r = new Random();
+      int chance = r.nextInt(2);
+      if (chance == 1) {
+         return"tails";
+      } else {
+         return"heads";
+      }
+   }
+}
+
+public class HeadsOrTails {
+   public static void main(String[] args) {
+      Toss t = new Toss();
+      int heads = 0;
+      int tails = 0;
+      int chances = 3;
+      System.out.println("Tossing a coin...");
+      for (int i = 1; i<= chances; i++) {
+	  System.out.print("Round"+i+":");
+         if (t.chanceFunc().equals("tails")) {
+            tails++;
+	    System.out.println("Tails");
+         } else {
+            heads++;
+	    System.out.println("Heads");
+         }
+      }
+      System.out.println("Heads: " + heads +", Tails:"+ tails);
+   }
+}
